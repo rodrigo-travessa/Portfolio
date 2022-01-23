1578068884bfe049f4cea9ced249036f98f37924
Rodrigo Travessa <rodrigosigiliao@outlook.com>
2022-01-23 11:24:07 -0300

    Add project files.

---
+++ Portfolio/Controllers/DogController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Portfolio.Models;
+
+
+namespace Portfolio.Controllers
+{
+    public class DogController : Controller
+    {
+        public string _listAllBreeds = "https://api.thedogapi.com/v1/breeds";
+        public string _searchByBreed = "https://api.thedogapi.com/v1/breeds/search?q=";
+        public string _apiKey = "88839931-955c-484a-92eb-f4425ba44529";
+
+        public async Task<IActionResult> Index()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-api-key", _apiKey);
+
+            var conteudo = await client.GetStringAsync(_listAllBreeds);
+            
+            List<DogModel> DogList = JsonConvert.DeserializeObject<List<DogModel>>(conteudo);
+            
+            return View(DogList);
+        }
+
+        
+        public async Task<IActionResult> GetByBreed(string Name)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("x-api-key", _apiKey);
+            
+            string searchLink = _searchByBreed + Name;
+
+            var conteudo = await client.GetStringAsync(searchLink);
+
+            List<DogModel> DogList = JsonConvert.DeserializeObject<List<DogModel>>(conteudo);
+            DogList.First().ImageURL = "https://cdn2.thedogapi.com/images/" + DogList[0].ImageId +".jpg";
+
+            return View("SpecificDog", DogList);
+        }
+    }
+}
+
---
+++ Portfolio/Models/DogModel.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+
+namespace Portfolio.Models
+{
+    public class DogModel
+    {
+        [JsonProperty("id")]
+        
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+       
+        public string Name { get; set; }
+
+        [JsonProperty("life_span")]
+        
+        public string LifeSpan { get; set; }
+
+        [JsonProperty("origin")]
+        
+        public string Origin { get; set; }
+
+        [JsonProperty("reference_image_id")]
+        
+        public string ImageId { get; set; }
+
+        [JsonProperty("temperament")]
+        
+        public string Temperament { get; set; }
+
+        [JsonProperty("weight")]
+        
+        public WeightRange Weight { get; set; }
+
+        [JsonProperty("height")]
+        
+        public HeightRange Height { get; set; }
+
+        public string ImageURL { get; set; } = "";   
+
+        
+    }
+
+    public class WeightRange
+    {
+        [JsonProperty("imperial")]
+        public string Imperial { get; set; }
+
+
+        [JsonProperty("metric")]
+        public string Metric { get; set; }
+    }
+    public class HeightRange
+    {
+        [JsonProperty("imperial")]
+        public string Imperial { get; set; }
+
+
+        [JsonProperty("metric")]
+        public string Metric { get; set; }
+    }
+}
